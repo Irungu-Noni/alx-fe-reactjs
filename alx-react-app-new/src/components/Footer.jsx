2bdcc5d0c206fa29ffa51bee218598fc598b87c7
Irungu-Noni <kamsycally@gmail.com>
2025-09-05 22:45:43 +0300

    Interacting with React is Fun

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p style={{ fontFamily: "sans-serif", fontSize: '13px' }}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main>
+            <p>I love to visit <i style={{ color: "skyblue" }}>New York, Paris, and Tokyo.</i></p>
+        </main>
+    );
+}
+
+export default MainContent
