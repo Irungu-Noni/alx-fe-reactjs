e36eece14c27c8be6f7bd0338bfce99541799cde
Irungu-Noni <kamsycally@gmail.com>
2025-09-07 16:29:42 +0300

    styling check

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,11 @@
 function MainContent() {
     return (
         <main>
-            <p>"I love to visit <i style={{ color: "skyblue" }}>New York, Paris, and Tokyo."</i></p>
+            <p>"I love to visit New York, Paris, and Tokyo."</p>
         </main>
     );
 }
 
 export default MainContent
+
+// {/* <p>"I love to visit /*<i style={{ color: "skyblue" }}> */ New York, Paris, and Tokyo."</i></p> */}
