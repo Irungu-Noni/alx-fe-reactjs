7c02db77cfbc086c69ba78fba66e09303ce32337
Irungu-Noni <kamsycally@gmail.com>
2025-09-07 20:28:32 +0300

    styling the navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
+    <nav style={{ padding: "15px", backgroundColor: "#333", display: "flex", justifyContent: "center", gap: "20px" }}>
       {/* The Links are like clickable doors to each page */}
       <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
       <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
