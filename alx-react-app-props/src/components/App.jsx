0f1e18c17e92f34a863d6b041144edd1d79929c0
Irungu-Noni <kamsycally@gmail.com>
2025-09-07 20:54:47 +0300

    Profile file

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,15 +1,12 @@
 import UserContext from './UserContext';
-// import ProfilePage from './ProfilePage';
+import ProfilePage from './ProfilePage';  
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <div>
-        <p>Name: {userData.name}</p>
-        <p>Email: {userData.email}</p>
-      </div>
+      <ProfilePage />
     </UserContext.Provider> 
   );
 }
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import UserContext from "./UserContext";
 
 function UserDetails() {
-
   const userData = useContext(UserContext);
   
   return (
