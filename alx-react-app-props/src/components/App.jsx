a6deaad657669821309f20a2c550fe28ddbf43e0
Irungu-Noni <kamsycally@gmail.com>
2025-09-07 17:04:53 +0300

    testing the file

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,14 +1,23 @@
 import UserContext from './UserContext';
-import ProfilePage from './ProfilePage';
+// import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <div>
+        <p>Name: {userData.name}</p>
+        <p>Email: {userData.email}</p>
+      </div>
     </UserContext.Provider> 
   );
 }
 
 export default App;
+
+// return (
+//     <UserContext.Provider value={userData}>
+//       <ProfilePage />
+//     </UserContext.Provider> 
+//   );
