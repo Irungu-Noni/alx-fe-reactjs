38fefd478ec242939911da54cf8bda49cd0f3607
Irungu-Noni <kamsycally@gmail.com>
2025-09-29 09:56:58 +0300

    Setting up the application with tailwindcss

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ './src/**/*.{js,ts,jsx,tsx}', '.public/index.html' ], darkMode: 'false',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
