4eb1cc697233378c78511a3b786a974040ec5e15
Irungu-Noni <kamsycally@gmail.com>
2025-10-04 22:50:00 +0300

    Modified the file some more

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ './src/**/*.{js,ts,jsx,tsx}', './public/index.html' ], darkMode: 'false',
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], darkMode: false,
   theme: {
     extend: {},
   },
