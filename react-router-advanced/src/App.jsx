e54d1f6ed08d2aee06cad3113c100271c875148a
Irungu-Noni <kamsycally@gmail.com>
2025-10-10 21:34:24 +0300

    Correctly named the protection route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -8,7 +8,7 @@ import Login from './pages/Login';
 import UserProfile from './pages/UserProfile';
 import Profile from './components/Profile';
 import BlogPost from './pages/BlogPost';
-import ProtectionRoute from './components/ProtectionRoute';
+import ProtectionRoute from './components/ProtectedRoute';
 
 function App() {
   return (
